c819629e073e582291ed6642999294ea651c9932
varuntyagiG <varungauravtyagi@gmail.com>
2025-01-24 01:30:06 +0530

    add

--- middle.cpp
+++ middle.cpp
@@ -2,15 +2,16 @@
 #include <stack>
 using namespace std;
 
-void findMiddle(stack<int> &st, int &Totalsize) {
-  if ((Totalsize / 2) + 1 == st.size()) {
-    cout << st.top() << " ";
+void findMiddle(stack<int> &st, int &totalSize) {
+  if ((totalSize / 2) + 1 == st.size()) {
+    cout << "Middle Element is : " << st.top() << " ";
     return;
   }
 
   int temp = st.top();
   st.pop();
-  findMiddle(st, Totalsize);
+
+  findMiddle(st, totalSize);
 
   st.push(temp);
 }
@@ -22,9 +23,9 @@ int main() {
     st.push(i);
   }
 
-  int Totalsize = st.size();
+  int totalSize = st.size();
 
-  findMiddle(st, Totalsize);
+  findMiddle(st, totalSize);
   cout << endl;
   return 0;
 }
